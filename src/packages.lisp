33695fb1402e25727578d7f5c6de5a842df3cc90
Jianshi Huang <huang@msi.co.jp>
2010-03-16 12:10:55 +0900

    remove nicknames

--- src/packages.lisp
+++ src/packages.lisp
@@ -2,7 +2,6 @@
 
 (defpackage :msi.ffi-utils
   (:use :cl :alexandria)
-  (:nicknames :ffi-utils)
   (:export #:complex-float
 	   #:complex-double
 	   #:defcfun
@@ -14,7 +13,6 @@
 
 (defpackage :msi.blas-lapack-common
   (:use :cl :alexandria :msi.ffi-utils)
-  (:nicknames :blas-lapack-common)
   (:export #:blas-int
 	   #:+precision-definitions+
 	   #:+matrix-type-definitions+
@@ -23,11 +21,9 @@
 	   ))
 
 (defpackage :msi.blas
-  (:use :cl :alexandria :msi.ffi-utils :msi.blas-lapack-common)
-  (:nicknames :blas))
+  (:use :cl :alexandria :msi.ffi-utils :msi.blas-lapack-common))
 
 (defpackage :msi.lapack
-  (:use :cl :alexandria :msi.ffi-utils :msi.blas-lapack-common)
-  (:nicknames :lapack))
+  (:use :cl :alexandria :msi.ffi-utils :msi.blas-lapack-common))
 
 
